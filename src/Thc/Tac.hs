417fde5ce1371c9a456af87017f6c20ca9c5ff18
elpinal <elpinal@users.noreply.github.com>
2017-12-07 20:03:19 +0900

    Enable to translate Expr to Tac

--- src/Thc/Tac.hs
+++ src/Thc/Tac.hs
@@ -1,5 +1,12 @@
 module Thc.Tac where
 
-data Tac = Copy Val Val
+import qualified Thc.Expr as Expr
+
+data Tac = Ret Val
+  deriving (Eq, Show)
 
 data Val = Var String
+  deriving (Eq, Show)
+
+fromExpr :: Expr.Term -> Tac
+fromExpr (Expr.Var i) = Ret $ Var i
--- test/Thc/TacSpec.hs
+++ test/Thc/TacSpec.hs
@@ -2,5 +2,12 @@ module Thc.TacSpec where
 
 import Test.Hspec
 
+import qualified Thc.Expr as Expr
+import Thc.Tac
+
 spec :: Spec
-spec = return ()
+spec = do
+  describe "fromExpr" $
+    it "translates Expr to Tac" $ do
+      fromExpr (Expr.Var "x") `shouldBe` Ret (Var "x")
+      fromExpr (Expr.Var "7") `shouldBe` Ret (Var "7")
