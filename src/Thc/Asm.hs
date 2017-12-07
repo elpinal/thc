4569f7249b5bf42d1bfaeb03a5f44b80ae25930c
elpinal <elpinal@users.noreply.github.com>
2017-12-07 20:14:46 +0900

    Add fromTac

--- src/Thc/Asm.hs
+++ src/Thc/Asm.hs
@@ -1,5 +1,13 @@
-module Thc.Asm where
+module Thc.Asm
+  ( Asm(..)
+  , fromTac
+  , Tac.Val(..)
+  ) where
 
-data Asm = Copy Loc Loc
+import qualified Thc.Tac as Tac
 
-data Loc = Mem Int
+data Asm = Ret Tac.Val
+  deriving (Eq, Show)
+
+fromTac :: Tac.Tac -> Asm
+fromTac (Tac.Ret v) = Ret v
---
+++ test/Thc/AsmSpec.hs
@@ -0,0 +1,12 @@
+module Thc.AsmSpec where
+
+import Test.Hspec
+
+import Thc.Asm
+import qualified Thc.Tac as Tac
+
+spec :: Spec
+spec = do
+  describe "fromTac" $
+    it "translates Tac to Asm" $ do
+      fromTac (Tac.Ret $ Var "x") `shouldBe` Ret (Var "x")
