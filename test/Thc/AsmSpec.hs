92f4d267831028ca1a30f46f28f2147ade6b091b
elpinal <elpinal@users.noreply.github.com>
2017-12-08 23:47:07 +0900

    Fix test

--- test/Thc/AsmSpec.hs
+++ test/Thc/AsmSpec.hs
@@ -9,4 +9,4 @@ spec :: Spec
 spec = do
   describe "fromTac" $
     it "translates Tac to Asm" $ do
-      fromTac (Tac.Ret $ Var "x") `shouldBe` Ret (Var "x")
+      fromTac (Tac.Ret $ Var "x") [] `shouldBe` [Ret (Var "x")]
