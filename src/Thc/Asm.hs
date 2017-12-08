662bc180ae5c97b133425d33b6eaaff12cdd8eb5
elpinal <elpinal@users.noreply.github.com>
2017-12-08 23:42:20 +0900

    Add Loc to Asm

--- src/Thc/Asm.hs
+++ src/Thc/Asm.hs
@@ -9,5 +9,8 @@ import qualified Thc.Tac as Tac
 data Asm = Ret Tac.Val
   deriving (Eq, Show)
 
+data Loc = StringTable Int
+         | Static Int
+
 fromTac :: Tac.Tac -> Asm
 fromTac (Tac.Ret v) = Ret v
