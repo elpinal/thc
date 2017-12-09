2a907e1ca12043914ea4a1b35df4813cab9497f2
elpinal <elpinal@users.noreply.github.com>
2017-12-09 23:47:40 +0900

    Update 'ret'

--- src/Thc/Code/Amd64.hs
+++ src/Thc/Code/Amd64.hs
@@ -1,5 +1,7 @@
 module Thc.Code.Amd64 where
 
+import qualified Data.ByteString.Lazy.Char8 as C
+import qualified Data.ByteString.Lazy as B
 import Data.Word
 
 import Thc.Asm
@@ -11,4 +13,4 @@ instance Machine Amd64 where
   fromAsm (Ret l, x) = ret l x
 
 ret :: Loc -> String -> Amd64 Code
-ret (StringTable n) x = Amd64 undefined
+ret (StringTable n) x = Amd64 $ C.pack x
