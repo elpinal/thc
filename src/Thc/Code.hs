15e07fdba88f32e0808ba9124110b2ffe1a4707f
elpinal <elpinal@users.noreply.github.com>
2017-12-09 23:47:04 +0900

    Make Code ByteString

--- src/Thc/Code.hs
+++ src/Thc/Code.hs
@@ -1,10 +1,11 @@
 module Thc.Code where
 
+import qualified Data.ByteString.Lazy as B
 import Data.Word
 
 import Thc.Asm
 
-type Code = [Word8]
+type Code = B.ByteString
 
 class Machine m where
   fromAsm :: Asm -> m Code
