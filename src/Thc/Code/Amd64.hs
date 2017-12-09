448c605a2d298c4aa745837ac3c8c29d98191f38
elpinal <elpinal@users.noreply.github.com>
2017-12-09 23:43:02 +0900

    Update fromAsm

--- src/Thc/Code/Amd64.hs
+++ src/Thc/Code/Amd64.hs
@@ -1,9 +1,14 @@
 module Thc.Code.Amd64 where
 
+import Data.Word
+
 import Thc.Asm
 import Thc.Code
 
 data Amd64 a = Amd64 a
 
 instance Machine Amd64 where
-  fromAsm (i, x) = Amd64 []
+  fromAsm (Ret l, x) = ret l x
+
+ret :: Loc -> String -> Amd64 Code
+ret (StringTable n) x = Amd64 undefined
