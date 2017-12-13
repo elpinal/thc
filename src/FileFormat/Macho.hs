d1f3b5abd651a3409a9b805a04684372b2937b3e
elpinal <elpinal@users.noreply.github.com>
2017-12-13 21:37:44 +0900

    Support encoding Mach-O file
    This implementation is currently insignificant.

--- src/FileFormat/Macho.hs
+++ src/FileFormat/Macho.hs
@@ -1,7 +1,9 @@
-{-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
 
 module FileFormat.Macho where
+
+import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.Lazy.Char8 as C
 
 import Data.Bits
 import Data.Word
@@ -9,19 +11,22 @@ import Data.Word
 class Encode a where
   encode :: a -> [Word8]
 
-instance (FiniteBits a, Integral a) => Encode a where
-  encode n = map (fromIntegral . (.&. 0xff)) . take b $ iterate shiftR8 n
-    where
-      b :: Int
-      b = finiteBitSize n `div` 8
+encodeBits :: (FiniteBits a, Integral a) => a -> [Word8]
+encodeBits n = map (fromIntegral . (.&. 0xff)) . take b $ iterate shiftR8 n
+  where
+    b :: Int
+    b = finiteBitSize n `div` 8
 
-      shiftR8 :: Bits a => a -> a
-      shiftR8 = flip shiftR 8
+    shiftR8 :: Bits a => a -> a
+    shiftR8 = flip shiftR 8
 
 data File = File
   { header   :: Header
   , segments :: [Segment]
   }
+
+instance Encode File where
+  encode File {header = h, segments = ss} = encodeHeader h ++ concatMap encodeSegment ss
 
 data Header = Header
   { magic      :: Word32
@@ -30,6 +35,18 @@ data Header = Header
   , filetype   :: FileType
   , hflags     :: Word32
   }
+
+encodeHeader :: Header -> [Word8]
+encodeHeader Header
+  { magic      = m
+  , cputype    = c
+  , cpusubtype = cs
+  , filetype   = f
+  , hflags     = h
+  } = concatMap encodeBits [m, c, cs] ++ encode f ++ encodeBits h ++ reserved
+  where
+    reserved :: [Word8]
+    reserved = [0, 0, 0, 0]
 
 data Segment = Segment
   { segname  :: String
@@ -41,19 +58,42 @@ data Segment = Segment
   , segflags :: Word32
   }
 
+encodeSegment :: Segment -> [Word8]
+encodeSegment Segment
+  { segname  = n
+  , maddr    = a
+  , msize    = s
+  , maxprot  = mp
+  , initprot = ip
+  , sections = ss
+  , segflags = fs
+  } = B.unpack (C.pack n) ++ concatMap encodeBits [a, s] ++ concatMap encode [mp, ip] ++ concatMap encodeSection ss ++ encodeBits fs
+
 data Section = Section
-  { secname    :: String
-  , addr       :: Word64
-  , size       :: Word64
-  , align      :: Word32
-  , secflags   :: Word32
+  { secname  :: String
+  , addr     :: Word64
+  , size     :: Word64
+  , align    :: Word32
+  , secflags :: Word32
   }
+
+encodeSection :: Section -> [Word8]
+encodeSection Section
+  { secname  = n
+  , addr     = a
+  , size     = s
+  , align    = al
+  , secflags = fs
+  } = B.unpack (C.pack n) ++ concatMap encodeBits [a, s] ++ concatMap encodeBits [al, fs]
 
 data Prot =
     Readable
   | Writable
   | Executable
   deriving Enum
+
+instance Encode [Prot] where
+  encode = encodeBits . foldr (.&.) 0 . map (\p -> (2 :: Word32) ^ fromEnum p)
 
 allProt :: [Prot]
 allProt = [Readable, Writable, Executable]
@@ -76,7 +116,7 @@ data FileType =
   deriving Enum
 
 instance Encode FileType where
-  encode f = encode $ (2 :: Word32) ^ fromIntegral (fromEnum f)
+  encode f = encodeBits $ (2 :: Word32) ^ fromIntegral (fromEnum f)
 
 lcSegment64 :: Word32
 lcSegment64 = 0x19
