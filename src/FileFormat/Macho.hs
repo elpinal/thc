0c42ca46665be195c8c54fdb6f091ec9fcb03ba5
elpinal <elpinal@users.noreply.github.com>
2017-12-13 17:53:11 +0900

    Annotate types for constants

--- src/FileFormat/Macho.hs
+++ src/FileFormat/Macho.hs
@@ -18,13 +18,16 @@ instance (FiniteBits a, Integral a) => Encode a where
       shiftR8 :: Bits a => a -> a
       shiftR8 = flip shiftR 8
 
+magicNumber64 :: Word32
 magicNumber64 = 0xfeedfacf
 
+abi64 :: Word32
 abi64 = 0x01000000
 
 amd64 :: Word32
 amd64 = 7 .|. abi64
 
+amd64All :: Word32
 amd64All = 3
 
 data FileType =
@@ -35,10 +38,14 @@ data FileType =
 instance Encode FileType where
   encode f = encode $ (2 :: Word32) ^ fromIntegral (fromEnum f)
 
+lcSegment64 :: Word32
 lcSegment64 = 0x19
 
+lcSymtab :: Word32
 lcSymtab = 0x02
 
+lcDysymtab :: Word32
 lcDysymtab = 0x0b
 
+lcUuid :: Word32
 lcUuid = 0x1b
