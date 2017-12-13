19e781e1ab5253f842654f35b527c1a7677f311d
elpinal <elpinal@users.noreply.github.com>
2017-12-13 17:46:58 +0900

    Add Encode type class

--- src/FileFormat/Macho.hs
+++ src/FileFormat/Macho.hs
@@ -1,7 +1,22 @@
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+
 module FileFormat.Macho where
 
 import Data.Bits
 import Data.Word
+
+class Encode a where
+  encode :: a -> [Word8]
+
+instance (FiniteBits a, Integral a) => Encode a where
+  encode n = map (fromIntegral . (.&. 0xff)) . take (l `div` 8) $ iterate shiftR8 n
+    where
+      l :: Int
+      l = finiteBitSize n
+
+      shiftR8 :: Bits a => a -> a
+      shiftR8 = flip shiftR 8
 
 magicNumber64 = 0xfeedfacf
 
