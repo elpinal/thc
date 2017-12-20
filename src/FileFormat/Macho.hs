965817ddc21f8b437f2ec337d6f2d277bc937158
elpinal <elpinal@users.noreply.github.com>
2017-12-20 21:23:57 +0900

    Change format

--- src/FileFormat/Macho.hs
+++ src/FileFormat/Macho.hs
@@ -366,30 +366,30 @@ encodeThreadState ThreadState
   , fs     = x19
   , gs     = x20
   } = concatMap encodeBits
-  [ unixThread
-  , threadStateLoadCommandSize
-  , amd64ThreadState
-  , amd64ExceptionStateCount
-  ] ++ concatMap encodeBits
-  [ x0
-  , x1
-  , x2
-  , x3
-  , x4
-  , x5
-  , x6
-  , x7
-  , x8
-  , x9
-  , x10
-  , x11
-  , x12
-  , x13
-  , x14
-  , x15
-  , x16
-  , x17
-  , x18
-  , x19
-  , x20
-  ]
+    [ unixThread
+    , threadStateLoadCommandSize
+    , amd64ThreadState
+    , amd64ExceptionStateCount
+    ] ++ concatMap encodeBits
+    [ x0
+    , x1
+    , x2
+    , x3
+    , x4
+    , x5
+    , x6
+    , x7
+    , x8
+    , x9
+    , x10
+    , x11
+    , x12
+    , x13
+    , x14
+    , x15
+    , x16
+    , x17
+    , x18
+    , x19
+    , x20
+    ]
