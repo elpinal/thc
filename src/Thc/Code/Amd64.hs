8ebaa924a4625fd3fb3e29f6c98f1f1d69410c8c
elpinal <elpinal@users.noreply.github.com>
2017-12-21 23:42:58 +0900

    Improve Context handling

--- src/Thc/Code/Amd64.hs
+++ src/Thc/Code/Amd64.hs
@@ -1,4 +1,6 @@
-module Thc.Code.Amd64 where
+module Thc.Code.Amd64
+  ( updateContext
+  ) where
 
 import qualified Data.ByteString.Lazy.Char8 as C
 import qualified Data.ByteString.Lazy as B
@@ -6,6 +8,9 @@ import Data.Word
 
 import Thc.Asm
 import Thc.Code
+
+updateContext :: Context -> Context
+updateContext c = c { cpu = \Amd64 -> return . fromAsm }
 
 fromAsm :: Asm -> Code
 fromAsm (Ret l, x) = ret l x
--- src/Thc/Compiler.hs
+++ src/Thc/Compiler.hs
@@ -2,8 +2,15 @@ module Thc.Compiler where
 
 import Thc.Asm
 import Thc.Code
+import qualified Thc.Code.Amd64 as Amd64
 import Thc.Expr
 import Thc.Tac
 
-compile :: Context -> Term -> OS -> CPU -> Either Error Code
-compile ctx t o c = encode ctx (fromTac $ fromExpr t) o c
+coreContext :: Context
+coreContext = Amd64.updateContext context
+
+compile :: Term -> OS -> CPU -> Either Error Code
+compile = compileWithContext coreContext
+
+compileWithContext :: Context -> Term -> OS -> CPU -> Either Error Code
+compileWithContext ctx t o c = encode ctx (fromTac $ fromExpr t) o c
