ea60d38b54e1722b3d1293c02b203cefdf53f06b
elpinal <elpinal@users.noreply.github.com>
2017-12-23 23:09:17 +0900

    Add CompilerSpec

---
+++ test/Thc/CompilerSpec.hs
@@ -0,0 +1,16 @@
+module Thc.CompilerSpec where
+
+import Test.Hspec
+
+import qualified Data.ByteString.Lazy as B
+
+import Thc.Code
+import Thc.Compiler
+import Thc.Expr
+
+spec :: Spec
+spec = do
+  describe "compile" $
+    it "compiles" $ do
+      want <- B.readFile "test/Thc/data/exit1"
+      compile (Var "a") Darwin Amd64 `shouldBe` Right want
