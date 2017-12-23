27fdbbccfa9e975b8af918f29dbff62e471a6529
elpinal <elpinal@users.noreply.github.com>
2017-12-23 23:55:11 +0900

    Implement fromExpr for Expr.App

--- src/Thc/Expr.hs
+++ src/Thc/Expr.hs
@@ -3,3 +3,4 @@ module Thc.Expr where
 data Term =
     Var String
   | Abs String Term
+  | App Term Term
--- src/Thc/Tac.hs
+++ src/Thc/Tac.hs
@@ -14,3 +14,13 @@ data FromExprError = OnlyAbs
 fromExpr :: Expr.Term -> Either FromExprError Tac
 fromExpr (Expr.Var i) = Right . Ret $ Var i
 fromExpr (Expr.Abs i e) = Left OnlyAbs
+fromExpr (Expr.App (Expr.Abs i e1) e2) = fromExpr $ replace e1 i e2
+
+replace :: Expr.Term -> String -> Expr.Term -> Expr.Term
+replace v @ (Expr.Var i) r e2
+  | i == r    = e2
+  | otherwise = v
+replace a @ (Expr.Abs i e1) r e2
+  | i == r    = a
+  | otherwise = Expr.Abs i $ replace e1 r e2
+replace a @ (Expr.App e1 e2) r e3 = Expr.App (replace e1 r e3) (replace e2 r e3)
