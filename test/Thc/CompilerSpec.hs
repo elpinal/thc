63ec31bf8b7634a4c8560f140e6825fd80df19f5
elpinal <elpinal@users.noreply.github.com>
2017-12-23 23:11:39 +0900

    Change test message

--- test/Thc/CompilerSpec.hs
+++ test/Thc/CompilerSpec.hs
@@ -11,6 +11,6 @@ import Thc.Expr
 spec :: Spec
 spec = do
   describe "compile" $
-    it "compiles" $ do
+    it "compiles a Term to an executable binary file" $ do
       want <- B.readFile "test/Thc/data/exit1"
       compile (Var "a") Darwin Amd64 `shouldBe` Right want
