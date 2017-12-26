24f83a96e3a7720e9f6216c43abf321e044a1765
elpinal <elpinal@users.noreply.github.com>
2017-12-26 20:37:53 +0900

    Add 'eval'

--- src/Thc/Expr.hs
+++ src/Thc/Expr.hs
@@ -4,3 +4,16 @@ data Term =
     Var String
   | Abs String Term
   | App Term Term
+
+eval :: Term -> Term
+eval t = case eval1 t of
+  Just t' -> eval t'
+  Nothing -> t
+
+eval1 :: Term -> Maybe Term
+eval1 (App (Abs i t1) t2) = return $ subst i t2 t1
+eval1 (App t1 t2) = flip App t2 <$> eval1 t1
+eval1 _ = Nothing
+
+subst :: String -> Term -> Term -> Term
+subst i t2 t1 = undefined
