d58cc602c49f2f7e79d5bfe89fa2518e098ada8c
elpinal <elpinal@users.noreply.github.com>
2017-12-26 22:51:38 +0900

    Write some of 'subst'

--- src/Thc/Expr.hs
+++ src/Thc/Expr.hs
@@ -1,5 +1,7 @@
 module Thc.Expr where
 
+import qualified Data.Set as Set
+
 data Term =
     Var String
   | Abs String Term
@@ -16,4 +18,15 @@ eval1 (App t1 t2) = flip App t2 <$> eval1 t1
 eval1 _ = Nothing
 
 subst :: String -> Term -> Term -> Term
-subst i t2 t1 = undefined
+subst i t (Var i')
+  | i == i'   = t
+  | otherwise = Var i'
+subst i t (App t1 t2) = subst i t t1 `App` subst i t t2
+subst i t (Abs i' t')
+  | i == i' = Abs i' t'
+  | i' `Set.notMember` fv t = Abs i' $ subst i t t'
+
+fv :: Term -> Set.Set String
+fv (Var i) = Set.singleton i
+fv (Abs i t) = Set.delete i $ fv t
+fv (App t1 t2) = fv t1 `Set.union` fv t2
