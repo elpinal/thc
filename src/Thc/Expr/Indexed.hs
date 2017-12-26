34ca7a50a412455491ac57cd9d94e9f60b247344
elpinal <elpinal@users.noreply.github.com>
2017-12-26 23:13:42 +0900

    Change the order of the parameters of Var

--- src/Thc/Expr/Indexed.hs
+++ src/Thc/Expr/Indexed.hs
@@ -1,7 +1,7 @@
 module Thc.Expr.Indexed where
 
 data Term =
-    Var Int Int String
+    Var String Int Int
   | Abs String Term
   | App Term Term
 
@@ -9,8 +9,8 @@ shift :: Int -> Term -> Term
 shift d t = walk 0 t
   where
     walk :: Int -> Term -> Term
-    walk c (Var x n i)
-      | x >= c    = Var (x + d) (n + d) i -- free
-      | otherwise = Var x (n + d) i
+    walk c (Var i x n)
+      | x >= c    = Var i (x + d) (n + d) -- free
+      | otherwise = Var i x $ n + d       -- bound
     walk c (Abs i t') = Abs i $ walk (c + 1) t'
     walk c (App t1 t2) = App (walk c t1) (walk c t2)
