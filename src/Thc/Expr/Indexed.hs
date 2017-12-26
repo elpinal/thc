583d3fe1d0a4eefeb9373f4b98774663ba7a9660
elpinal <elpinal@users.noreply.github.com>
2017-12-26 23:12:11 +0900

    Add 'shift' for Indexed.Term

--- src/Thc/Expr/Indexed.hs
+++ src/Thc/Expr/Indexed.hs
@@ -4,3 +4,13 @@ data Term =
     Var Int Int String
   | Abs String Term
   | App Term Term
+
+shift :: Int -> Term -> Term
+shift d t = walk 0 t
+  where
+    walk :: Int -> Term -> Term
+    walk c (Var x n i)
+      | x >= c    = Var (x + d) (n + d) i -- free
+      | otherwise = Var x (n + d) i
+    walk c (Abs i t') = Abs i $ walk (c + 1) t'
+    walk c (App t1 t2) = App (walk c t1) (walk c t2)
