79e08d3bdb1be7668f8dbaecd80cfd50d3230c75
elpinal <elpinal@users.noreply.github.com>
2017-12-28 20:08:17 +0900

    Test compiling of Int

--- test/Thc/CompilerSpec.hs
+++ test/Thc/CompilerSpec.hs
@@ -12,5 +12,8 @@ spec :: Spec
 spec = do
   describe "compile" $
     it "compiles a Term to an executable binary file" $ do
-      want <- B.readFile "test/Thc/data/exit1"
+      want <- B.readFile "test/Thc/data/exit178"
+      compile (Lit $ Int 178) Darwin Amd64 `shouldBe` Right want
+
+      want <- B.readFile "test/Thc/data/exit81"
       compile (Lit $ Bool True) Darwin Amd64 `shouldBe` Right want
