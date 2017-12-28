29c427d5daf8938af3ea512d031709b55b19907e
elpinal <elpinal@users.noreply.github.com>
2017-12-28 19:57:06 +0900

    Support Int in assembling

--- src/Thc/Code/Amd64.hs
+++ src/Thc/Code/Amd64.hs
@@ -22,13 +22,15 @@ fromAsm (Return' l) = ret l
 -- Use exit syscall to exit with code: False = 80, True = 81.
 -- Note that syscall numbers depend on OS.
 ret :: Literal -> Code
-ret (Bool b) = B.concat
+ret l = B.concat
   [ syscallNumber
   , B.pack $ 0xbf : encodeBits v ++ [0x0f, 0x05]
   ]
   where
     v :: Word32
-    v = fromIntegral $ 80 + fromEnum b
+    v = fromIntegral $ case l of
+      Bool b -> 80 + fromEnum b
+      Int i -> i
 
     -- | TODO: This depends on System V ABI and XNU.
     syscallNumber :: B.ByteString
