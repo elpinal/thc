271f78ed4c452b852f3c01dd5f53dcc317c806de
elpinal <elpinal@users.noreply.github.com>
2017-12-28 20:20:10 +0900

    Delete Tac'

--- src/Thc/Tac.hs
+++ src/Thc/Tac.hs
@@ -31,43 +31,6 @@ replace a @ (Expr.App e1 e2) r e3 = Expr.App (replace e1 r e3) (replace e2 r e3)
 
 -------------------
 
-type Tac' = ([Inst], [FnDecl])
-
-data Inst =
-    Assign String String
-  | App String String
-  | Return String
-  deriving (Eq, Show)
-
-data FnDecl = FnDecl String String [Inst]
-  deriving (Eq, Show)
-
-data FromExprError' = UndefinedVariable String
-  deriving (Eq, Show)
-
-fromExpr' :: Expr.Term -> StateT Int (Either FromExprError') (String, [Inst], [FnDecl])
-
-fromExpr' (Expr.Var i) = return (i, [], [])
-
-fromExpr' (Expr.Abs i t) = do
-  fi <- ("f" ++) . show <$> freshName -- FIXME: be likely to conflict
-  (a, is, fs) <- fromExpr' t
-  return (fi, is, fs ++ [FnDecl fi i [Return a]])
-
-fromExpr' (Expr.App t1 t2) = do
-  i <- ("a" ++) . show <$> freshName -- FIXME: be likely to conflict
-  (a, is1, fs1) <- fromExpr' t1
-  (b, is2, fs2) <- fromExpr' t2
-  return (i, is1 ++ is2 ++ [App a b], fs1 ++ fs2)
-
-freshName :: Monad m => StateT Int m Int
-freshName = do
-  n <- get
-  put $ n + 1
-  return n
-
--------------------
-
 data Tac'' = Return' I.Literal
 
 type Literal = I.Literal
