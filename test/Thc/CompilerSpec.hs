2e6ac88ba639cc73b623590eac06b190a52ace31
elpinal <elpinal@users.noreply.github.com>
2017-12-29 17:19:47 +0900

    Test an example of failure in compiling

--- test/Thc/CompilerSpec.hs
+++ test/Thc/CompilerSpec.hs
@@ -4,16 +4,20 @@ import Test.Hspec
 
 import qualified Data.ByteString.Lazy as B
 
-import Thc.Code
+import qualified Thc.Code as Code
 import Thc.Compiler
 import Thc.Expr
 
 spec :: Spec
 spec = do
-  describe "compile" $
+  describe "compile" $ do
     it "compiles a Term to an executable binary file" $ do
       want <- B.readFile "test/Thc/data/exit178"
-      compile (Lit $ Int 178) Darwin Amd64 `shouldBe` Right want
+      compile (Lit $ Int 178) Code.Darwin Code.Amd64 `shouldBe` Right want
 
       want <- B.readFile "test/Thc/data/exit81"
-      compile (Lit $ Bool True) Darwin Amd64 `shouldBe` Right want
+      compile (Lit $ Bool True) Code.Darwin Code.Amd64 `shouldBe` Right want
+
+    context "when given an invalid program" $
+      it "returns an error" $ do
+        compile (Var "x") Code.Darwin Code.Amd64 `shouldBe` Left Unbound
