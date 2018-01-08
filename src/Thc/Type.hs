f3e65fb6bd62feb705d31c7e3f5b799a02e6e1e5
elpinal <elpinal@users.noreply.github.com>
2018-01-08 16:03:09 +0900

    Remove 'subtype'

--- src/Thc/Type.hs
+++ src/Thc/Type.hs
@@ -1,7 +1,6 @@
 module Thc.Type
   ( Type(..)
   , variant
-  , subtype
   ) where
 
 import qualified Data.Map.Lazy as Map
@@ -21,10 +20,3 @@ infixr 9 :->:
 -- | @variant xs@ creates a new 'Variant' from @xs@.
 variant :: [(String, Type)] -> Type
 variant = Variant . Map.fromList
-
--- |
--- @subtype s t@ tests whether @s@ is subtype of @t@.
--- It returns @True@ if @s == t@.
-subtype :: Type -> Type -> Bool
-subtype (Variant ss) (Variant ts) = Map.isSubmapOf ss ts
-subtype s t = s == t
--- test/Thc/TypeSpec.hs
+++ test/Thc/TypeSpec.hs
@@ -5,16 +5,4 @@ import Test.Hspec
 import Thc.Type
 
 spec :: Spec
-spec = do
-  describe "subtype" $
-    it "tests subtyping" $ do
-      subtype Int Int  `shouldBe` True
-      subtype Int Bool `shouldBe` False
-
-      let nint = variant [("n", Int)]
-      subtype Int nint          `shouldBe` False
-      subtype nint nint         `shouldBe` True
-      subtype (variant []) nint `shouldBe` True
-
-      subtype (variant [("x", Int)]) nint  `shouldBe` False
-      subtype (variant [("n", Bool)]) nint `shouldBe` False
+spec = return ()
