cf1148fd0d6cf9cedebd180c3971bead8e9d844c
elpinal <elpinal@users.noreply.github.com>
2018-01-12 17:37:54 +0900

    Test varBind

--- test/Thc/TypeSpec.hs
+++ test/Thc/TypeSpec.hs
@@ -1,8 +1,30 @@
 module Thc.TypeSpec where
 
 import Test.Hspec
+import Test.QuickCheck
+
+import Control.Applicative
 
 import Thc.Type
 
+instance Arbitrary TypeId where
+  arbitrary = IdString <$> arbitrary
+
+instance Arbitrary Type where
+  arbitrary = oneof $
+    map return [Int] ++
+    [ Id <$> arbitrary
+    , liftA2 (:->:) arbitrary arbitrary
+    ]
+
 spec :: Spec
-spec = return ()
+spec = do
+  describe "varBind" $ do
+    context "when both types are the same" $ do
+      it "returns the empty substitution" $ property $ \i ->
+        varBind i (Id i) === return emptySubst
+
+    context "when both types are not the same" $ do
+      it "returns a substitution from the type variable to the type" $ property $
+        \i -> forAll (suchThat arbitrary (/= Id i)) $ \t ->
+          varBind i t === return (i |-> t)
