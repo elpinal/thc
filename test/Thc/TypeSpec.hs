15032bc4459d3717ff6fcd7e4ea4b308272484d2
elpinal <elpinal@users.noreply.github.com>
2018-01-13 10:38:28 +0900

    Test "mgu"

--- test/Thc/TypeSpec.hs
+++ test/Thc/TypeSpec.hs
@@ -44,5 +44,20 @@ spec = do
     context "when given two arrow type" $ do
       it "returns the most general unifier" $ do
         shouldCommute mgu (Int :->: idString "X") (Int :->: idString "X")          $ return emptySubst
-        shouldCommute mgu (Int :->: idString "X") (idString "X" :->: Int)          $ return (IdString "X" |-> Int)
+        shouldCommute mgu (Int :->: idString "X") (idString "X" :->: Int)          $ return $ IdString "X" |-> Int
         shouldCommute mgu (idString "Y" :->: idString "X") (idString "X" :->: Int) $ IdString "Y" |-> Int `merge` IdString "X" |-> Int
+        shouldCommute mgu (idString "X" :->: idString "Y") (Int :->: idString "X") $ IdString "Y" |-> Int `merge` IdString "X" |-> Int
+        shouldCommute mgu (idString "X" :->: idString "Y") (idString "X" :->: Int) $ return $ IdString "Y" |-> Int
+
+    context "when either or both of the two types are type variable" $ do
+      it "returns the result of varBind" $ do
+        shouldCommute mgu (idString "X") (idString "X") $ return emptySubst
+
+        mgu (idString "X") (idString "Y") `shouldBe` return (IdString "X" |-> idString "Y")
+        mgu (idString "Y") (idString "X") `shouldBe` return (IdString "Y" |-> idString "X")
+
+        shouldCommute mgu (idString "X" :->: idString "Y") (idString "X") $ return $ IdString "X" |-> (idString "X" :->: idString "Y")
+
+    context "when two types cannot be unified" $ do
+      it "returns an error" $ do
+        mgu (idString "X" :->: idString "Y") Int `shouldBe` Left (Unify (idString "X" :->: idString "Y") Int)
